959ea1683fba1f8b7188ea530ee6c92dd8acb10d
subhash <mr.subhash505@gmail.com>
2022-07-10 17:15:43 +0530

    modified sample

--- sample.py
+++ sample.py
@@ -1,2 +1,7 @@
 x=(71,23,45,23,564,32,18)
 print("the smallest number from the list is :",min(x))
+
+
+
+print("the largest number in the list is",max(x))
+print(" this is the updated statement in the program")
